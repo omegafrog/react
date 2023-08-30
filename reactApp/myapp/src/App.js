8a94c862b252c65db047552092afa65616d7c009
omegafrog <jiwoo22110103@gmail.com>
2023-08-30 15:22:45 +0900

    useEffect
    useEffect gets 2 argument.
    first argument is callback function that you want to run only once.
    Second argument is array that has variable you want to watch.
    If you set some variable in this array, callback function is run
    when that variable changes.

--- reactApp/myapp/src/App.js
+++ reactApp/myapp/src/App.js
@@ -1,10 +1,27 @@
-import Button from "./Button";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [counter, setCounter] = useState(0);
+  const [searchKeyword, setKeyword] = useState("");
+  const onClick = () => setCounter((cur) => (cur += 1));
+  const onChange = (event) => setKeyword(event.target.value);
+  useEffect(() => console.log("Calling api that i want run only once"), []);
+  useEffect(() => {
+    if (searchKeyword.length >= 1) {
+      console.log("searching...:", searchKeyword);
+    }
+  }, [searchKeyword]);
   return (
     <div>
       <h1>hi</h1>
-      <Button text="continue"></Button>
+      <input
+        type="text"
+        placeholder="search here"
+        value={searchKeyword}
+        onChange={onChange}
+      ></input>
+      <h3>{counter}</h3>
+      <button onClick={onClick}>click me</button>
     </div>
   );
 }
